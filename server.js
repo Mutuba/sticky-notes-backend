64cefedccf623a08a8d034dda0659839da548120
Mutuba <danielmutubait@gmail.com>
2024-09-09 13:46:44 +0300

    Remove hard coded port

--- server.js
+++ server.js
@@ -22,12 +22,12 @@ app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/notes", notesRoutes);
 
-// const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("MongoDB connected");
-    app.listen();
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
   })
   .catch((err) => console.error(err));
